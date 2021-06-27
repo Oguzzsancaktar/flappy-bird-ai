459fbc655df30f19e2dec7e814398c124206fc22
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-06-27 12:35:20 +0300

    first configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <canvas
+          id="myCanvas"
+          width="800"
+          height="500"
+          style={{ marginTop: "24px", border: "1px solid #c3c3c3" }}
+        ></canvas>
+      </div>
+    );
+  }
 }
 
 export default App;
